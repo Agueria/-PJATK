82573e24a2254170b5a98d5d01d58f8e98aff6d1
Yoshino <16629151+Agueria@users.noreply.github.com>
2021-10-30 13:34:33 +0300

    i fix converter
    30.10.2021 v2

--- Galon converter/src/com/company/Main.java
+++ Galon converter/src/com/company/Main.java
@@ -6,28 +6,33 @@ public class Main {
     public static void main(String[] args) {
         System.out.print("Enter fuel consumption and units (\"eu\" or \"usa\") ");
         Scanner sc = new Scanner(System.in);
-        int inputQuantity = sc.nextInt();
         String input = sc.nextLine();
         sc.close();
-
+        float coef = Float.parseFloat("235.214583");
+        boolean x = (input.contains("usa"));
+        boolean y = (input.contains("eu"));
 
         if (input.contains("usa")){
-            String inputunit = "usa";
-            System.out.println(inputunit);
-            //int usaToEu = inputQuantity
+            int inlen = input.length();
+            float inq = Float.parseFloat(input.substring(0,(inlen-4)));
+            //1 mpg = 235.21 l/100km
+            float usatoeu = coef/inq;
+            System.out.println(inq + " mpg = " + usatoeu + "L/100km");
         }
 
+        if (input.contains("eu")) {
 
-        if (input.contains("eu")){
-            String inputunit = "eu";
-            System.out.println(inputunit);
-            int euToUsa = inputQuantity*235;
-            System.out.println(euToUsa);
+            int inlen = input.length();
+            float inq = Float.parseFloat(input.substring(0, (inlen - 3)));
+            // 1 L/100km = 1/235.21 mpg
+            float eutousa = coef / inq;
+            System.out.println(inq + " L/100km = " + eutousa + " mpg");
+
         }
 
-
-        else{
+        if(x == y){
             System.out.println("Wrong input");
         }
+
     }
 }
